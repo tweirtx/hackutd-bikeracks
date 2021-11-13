4422c1f78c71d948a700e5973c90d1ecf6845d93
Travis Weir <tweirtx@gmail.com>
2021-11-13 16:43:15 -0600

    Wrote get methods

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -6,19 +6,25 @@ from openapi_server import encoder
 import asyncpg
 import asyncio
 
+global db_connection, loop
+
 
 async def initdb():
-    print("Initializing DB")
     url = "postgres://bikeracks:somethingclever@db:5432/bikeracks"
+    global db_connection
     db_connection = await asyncpg.create_pool(dsn=url)
     with open('schema.sql', 'r') as schema:
-        for line in schema.read().split(';'):
-            await db_connection.execute(line)
+        file_contents = schema.read()
+        schema_lines = file_contents.split(';')
+        for line in schema_lines:
+            if len(line) == 0:
+                await db_connection.execute(line)
 
 
 def main():
-    asyncio.run(initdb())
-    print("Database initialized!")
+    global loop
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(initdb())
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
--- server/openapi_server/controllers/default_controller.py
+++ server/openapi_server/controllers/default_controller.py
@@ -1,8 +1,25 @@
+import json.encoder
+
 import connexion
-import six
+import flask
 
 from openapi_server.models.location import Location  # noqa: E501
 from openapi_server import util
+from __main__ import db_connection, loop
+
+
+async def query_for(dbentry_id=None):
+    output = []
+    if dbentry_id is None:
+        results = await db_connection.fetch("SELECT * FROM locations")
+    else:
+        results = await db_connection.fetch("SELECT * FROM locations WHERE id = $1", dbentry_id)
+    for result in results:
+        resultout = {}
+        for item in result.items():
+            resultout[item[0]] = item[1]
+        output.append(resultout)
+    return json.encoder.JSONEncoder().encode(o=output)
 
 
 def addlocation_post(location):  # noqa: E501
@@ -17,6 +34,7 @@ def addlocation_post(location):  # noqa: E501
     """
     if connexion.request.is_json:
         location = Location.from_dict(connexion.request.get_json())  # noqa: E501
+        db_connection.execute()
     return 'do some magic!'
 
 
@@ -30,7 +48,11 @@ def location_location_id_get(location_id):  # noqa: E501
 
     :rtype: Location
     """
-    return 'do some magic!'
+    results = loop.run_until_complete(query_for(location_id))
+    if len(results) > 2:
+        return results
+    else:
+        return flask.abort(404)
 
 
 def locations_get():  # noqa: E501
@@ -41,4 +63,5 @@ def locations_get():  # noqa: E501
 
     :rtype: List[Location]
     """
-    return 'do some magic!'
+    results = loop.run_until_complete(query_for())
+    return results
